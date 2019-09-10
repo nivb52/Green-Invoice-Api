297b81fc6226cd38689ef37c344e68cf7df5145a
nivb52 <nivb52@gmail.com>
2019-09-11 01:16:25 +0300

    logo cmp using assets from mail

---
+++ src/cmps/Logo.vue
@@ -0,0 +1,14 @@
+<template>
+    <img width="100px" alt="green invoice logo" src="@/assets/green_logo.svg">
+</template>
+
+<script>
+export default {
+    //TODO : 
+    // GET PROPS OF THE SIZE OF THE LOGO
+}
+</script>
+
+<style>
+
+</style>
