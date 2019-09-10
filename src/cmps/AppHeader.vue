a3b88db4cf17b8bfa628dbff1623092e26d47fc5
nivb52 <nivb52@gmail.com>
2019-09-11 01:15:55 +0300

    create routes

---
+++ src/cmps/AppHeader.vue
@@ -0,0 +1,45 @@
+<template>
+  <div id="nav">
+    <div class="tiny">
+      debug :
+      <router-link to="/login">Login</router-link>&nbsp; | &nbsp;
+      <router-link to="/user-info">User Info</router-link>&nbsp; | &nbsp;
+      <router-link to="/">Home</router-link>&nbsp; | &nbsp;
+    </div>
+  </div>
+</template>
+
+<script>
+import Logo from "@/cmps/Logo.vue";
+
+export default {
+  name: "app-header",
+  components: {
+    Logo
+  }
+};
+</script>
+
+<style lang="scss">
+.tiny {
+  font-size: 0.6rem;
+}
+#nav {
+  position: absolute;
+  bottom: 0;
+  left: 0;
+  padding: 0;
+  margin: 0;
+
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+    &.logo {
+      margin-left: auto;
+    }
+  }
+}
+</style>
