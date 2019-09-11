630f6f8800fa1ca32ef458d638600f94054a8d94
nivb52 <nivb52@gmail.com>
2019-09-11 13:43:53 +0300

    render bussines details as list and preview

---
+++ src/cmps/BusinessesList.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+  <BusinessesPreview v-for="detail in businesses" v-show="detail !== null" :key="businesses.id" :detail="detail"  />
+  </div>
+</template>
+
+<script>
+import BusinessesPreview from "@/cmps/BusinessesPreview.vue"
+
+
+export default {
+  name: "businessesList",
+   components: {
+      BusinessesPreview
+  },
+  props: ["businesses"],
+
+};
+</script>
+
+<style>
+body {
+    overflow: auto;
+}
+</style>
---
+++ src/cmps/BusinessesPreview.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+   <li> {{detail}} </li>
+   
+  </div>
+</template>
+
+<script>
+export default {
+  name: "businessesPreview",
+ 
+  props: ["detail"]
+};
+</script>
+
+<style>
+</style>
